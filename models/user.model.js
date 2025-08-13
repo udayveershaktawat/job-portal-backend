88478b2b2cd74e7fc91723338d28bb49409c5170
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-13 23:32:49 +0530

    user schema completed

---
+++ models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true,
+
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phoneNumber:{
+        type:Number,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        enum:["student","recruiter"],
+        required:true,
+    },
+    profile:{
+        bio:{
+            type:String
+        },
+        skills:{
+            type:String
+        },
+        resume:{
+            type:String
+        },
+        resumeOriginalName:{
+            type:String
+        },
+        company:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'company'
+        },
+        profilePhoto:{
+            type:String,
+            default:''
+        }
+    }
+},{timestamps:true});
+
+
+module.exports = mongoose.model("User",userSchema)
