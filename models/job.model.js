3edc8d61cf9e90a16412ccaf63d73f01e60c2471
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-13 23:43:59 +0530

    job model created successfully

---
+++ models/job.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+
+
+const jobSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    requirements:[{
+        type:String,
+
+    }],
+    salary:{
+        type:Number,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true,
+    },
+    jobType:{
+        type:String,
+        required:true
+    },
+    position:{
+        type:Number,
+    },
+    company:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'company',
+        required:true,
+    },
+    created_by:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    applications:[{
+         type:mongoose.Schema.Types.ObjectId,
+        ref:'Application',
+        
+    }]
+});
+
+module.exports = mongoose.model("Job",jobSchema);
