626702e08cb93c2d6d03a291a7dab0596dfca78b
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-15 23:32:28 +0530

    app updated

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectWithDb = require("./config/database");
+const userRoute = require("./routes/user.route")
+
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin:'http//localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+connectWithDb();
+
+// api
+app.use("/api/v1",userRoute)
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server started at port number ${PORT}`)
+})
