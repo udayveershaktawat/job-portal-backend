18c853926df9c750bfaefeed8f6befc430341ac4
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-16 23:44:59 +0530

    get company by id

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -48,6 +48,34 @@ exports.getCompany = async(req,res)=>{
             })
         }
 
+        return res.status(200).json({
+            message:"company fetched successfully",
+            success:true
+        })
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+// get company by Id 
+exports.getCompanyById = async(req,res)=>{
+    try{
+        const companyId = req.params.id;
+        const company = await Company.findById({companyId});
+           if(!company){
+            return res.status(404).json({
+                success:false,
+                message:"company not found"
+            })
+        }
+
+        return res.status(200).json({
+            message:"company fetched successfully",
+            success:true
+        })
+
     }
     catch(error){
         console.log(error)
