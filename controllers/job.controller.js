d85a5c6895ab3aaf5c77c32be285803e84f83c46
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-17 23:56:46 +0530

    job fetched by id

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -69,3 +69,28 @@ exports.getAllJobs = async(req,res)=>{
         console.log(error)
     }
 }
+
+
+// get jobi by id
+exports.getJobById = async(req,res)=>{
+    try{
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId)
+        if(!job){
+            return res.status(404).json({
+                success:false,
+                message:"jobs not found"
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            message:"job fetched successfully through Id"
+        })
+
+    }
+    catch(error){
+        console.log(error)
+
+    }
+}
