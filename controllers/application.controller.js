c17389a649d6d03bf5436967ecafb5b9c366a6dc
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-20 00:06:25 +0530

    status updated successfully

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -116,3 +116,44 @@ exports.getApplicant = async(req,res)=>{
         console.log(error)
     }
 }
+
+// update status
+exports.updateStatus = async(req,res)=>{
+    try{
+        const status = req.body;
+        const applicationId = req.params.id;
+
+        if(!status){
+             return res.status(404).json({
+                message:"status is required",
+                success:false,
+            })
+        };
+
+        // find the application by applicant id
+
+        const application = await Application.findOne({_id:applicationId})
+        if(!application){
+            return res.status(404).json({
+                message:"application not found",
+                success:false,
+            })
+        }
+
+        // update the status
+        application.status = status.toLowerCase();
+        await application.save();
+
+
+        return res.status(200).json({
+            message:"status updated successfully",
+            success:true
+        })
+
+        
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
